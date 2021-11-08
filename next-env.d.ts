0c6dd352111ada81df314acf7ca52cc9b4c4d04f
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-11-07 21:12:48 -0300

    problems with webpack

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
